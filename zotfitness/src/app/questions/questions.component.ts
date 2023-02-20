58c45cbf380c5c8773e02bebba9b4bf6988e9b35
Mindy Jun <mindyjvn@gmail.com>
2023-02-19 22:47:02 -0800

    ignore this
    deleted a linet hat wasnt supposed to get pushed

--- zotfitness/src/app/questions/questions.component.ts
+++ zotfitness/src/app/questions/questions.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-questions',
