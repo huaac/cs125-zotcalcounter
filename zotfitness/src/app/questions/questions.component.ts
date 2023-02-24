2ea5ff08d56798834613a361cbf6ab571d76b9ab
huaac <huaac1@uci.edu>
2023-02-24 00:06:25 -0800

    Revert "Merge branch 'main' of https://github.com/huaac/cs125-zotfitness"
    This reverts commit f2a746fcd7b3d2011c4b3c592567e41f5acd6bed, reversing
    changes made to fefc451b35e7305760d9af8715b3ada7898f9e81.

--- zotfitness/src/app/questions/questions.component.ts
+++ zotfitness/src/app/questions/questions.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
-import { Router } from '@angular/router'; //added
-
 @Component({
   selector: 'app-questions',
   templateUrl: './questions.component.html',
@@ -11,11 +9,8 @@ import { Router } from '@angular/router'; //added
 })
 export class QuestionsComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor() { }
 
   ngOnInit() {}
 
-  onQuestionsClick() {
-    this.router.navigateByUrl('/search');
-  }
 }
