7b713941a5e762c71912c4f52f3632badd2fd0cb
worstdog <50923937+worstdog@users.noreply.github.com>
2023-02-24 14:59:48 -0500

    Update history.component.ts

--- zotfitness/src/app/history/history.component.ts
+++ zotfitness/src/app/history/history.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router'; //added
 
 @Component({
+  imports: [IonicModule],
   selector: 'app-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss'],
