9fb94b252e31960563932a78b3779a671cbf9cc5
huaac <huaac1@uci.edu>
2023-02-23 21:25:57 -0800

    Revert "Update history.component.ts"
    This reverts commit ec042a6ee95da4b0925b6b89f1f90e7994a01909.

--- zotfitness/src/app/history/history.component.ts
+++ zotfitness/src/app/history/history.component.ts
@@ -1,4 +1,3 @@
-//This code takes in two parameters: total workout minutes, daysoff, and prints out a monthly plan of workout minutes
 import { Component, OnInit } from '@angular/core';
 import * as readline from 'readline-sync';
 import { Router } from '@angular/router'; //added
@@ -9,40 +8,43 @@ import { Router } from '@angular/router'; //added
   styleUrls: ['./history.component.scss'],
 })
 export class HistoryComponent implements OnInit {
-  
-  const totalMinutes = Number(readline.question("Enter the total workout minutes for the month: "));
-  const offDays = readline.question("Enter the days off (separated by comma and space): ").split(", ");
-  const workDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].filter(day => !offDays.includes(day));
-  const dailyMinutes = Math.round(totalMinutes / workDays.length);
 
-  constructor(private router:Router) {}
+  constructor(private router:Router) { }
 
   ngOnInit() {}
-
-  // Determine the number of days in the month, accounting for the days off
-  let daysInMonth = 31;
-  for (let day of offDays) {
-    const index = workDays.indexOf(day);
-    if (index >= 0) {
-      workDays.splice(index, 1);
-      daysInMonth--;
-    }
-  }
-
-  // Initialize a dictionary to store the daily workout minutes
-  const schedule = {};
-  for (let day = 1; day <= daysInMonth; day++) {
-    const weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][(day - 1) % 7];
-    if (workDays.includes(weekday)) {
-      schedule[day] = [weekday, dailyMinutes];
-    } else {
-      schedule[day] = [weekday, 0];
-    }
-  }
-
-  // Print out the month schedule of daily workout minutes
-  console.log("Month Schedule of Daily Workout Minutes:");
-  for (const [day, [weekday, minutes]] of Object.entries(schedule)) {
-    console.log(`${day} ${weekday} : ${Math.round(minutes)} minutes`);
 }
+
+//This code takes in two parameters: total workout minutes, daysoff, and prints out a monthly plan of workout minutes
+
+
+const totalMinutes = Number(readline.question("Enter the total workout minutes for the month: "));
+const offDays = readline.question("Enter the days off (separated by comma and space): ").split(", ");
+const workDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].filter(day => !offDays.includes(day));
+const dailyMinutes = Math.round(totalMinutes / workDays.length);
+
+// Determine the number of days in the month, accounting for the days off
+let daysInMonth = 31;
+for (let day of offDays) {
+  const index = workDays.indexOf(day);
+  if (index >= 0) {
+    workDays.splice(index, 1);
+    daysInMonth--;
+  }
+}
+
+// Initialize a dictionary to store the daily workout minutes
+const schedule = {};
+for (let day = 1; day <= daysInMonth; day++) {
+  const weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][(day - 1) % 7];
+  if (workDays.includes(weekday)) {
+    schedule[day] = [weekday, dailyMinutes];
+  } else {
+    schedule[day] = [weekday, 0];
+  }
+}
+
+// Print out the month schedule of daily workout minutes
+console.log("Month Schedule of Daily Workout Minutes:");
+for (const [day, [weekday, minutes]] of Object.entries(schedule)) {
+  console.log(`${day} ${weekday} : ${Math.round(minutes)} minutes`);
 }
