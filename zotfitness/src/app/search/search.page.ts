ed8921ee225d7512bdd59495177ca3d6ec1e4deb
huaac <huaac1@uci.edu>
2023-02-25 17:31:33 -0800

    implemented search page ui

--- zotfitness/src/app/search/search.page.ts
+++ zotfitness/src/app/search/search.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { SearchService } from 'src/app/service/search.service'; // added
 import { Router } from '@angular/router'; //added
+import { InfiniteScrollCustomEvent, LoadingController } from '@ionic/angular'; //added
+
 
 @Component({
   selector: 'app-search',
@@ -10,14 +12,52 @@ import { Router } from '@angular/router'; //added
 })
 export class SearchPage implements OnInit {
 
-  constructor(private searchService: SearchService, private router:Router) { }
+  workouts: string[] = [];
+  currentPage = 1;
+//   imageBaseUrl = environment.images; // no images atm...
+
+  constructor(private searchService: SearchService, private router:Router, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
 //     this.searchService.getMuscleWorkouts(); // on initialization of page, print bicep data
+  this.loadWorkouts("")
   }
 
   onSearchClick() {
     this.router.navigateByUrl('/history');
+  }
+
+  async loadWorkouts(parameters: string, event?: InfiniteScrollCustomEvent) {
+    const loading = await this.loadingCtrl.create({
+      message: 'Loading..',
+      spinner: 'bubbles',
+    });
+    await loading.present();
+
+    this.workouts = [];
+
+    this.searchService.getMuscleWorkouts(parameters).subscribe(
+      (res) => {
+        loading.dismiss();
+        for (let i = 0; i < 10; i++)
+          this.workouts.push(res[i].name);
+
+      },
+      (err) => {
+        console.log(err);
+        loading.dismiss();
+      }
+    );
+  }
+
+  async searchbarChange(input: any){
+    //console.log(content.detail.value)
+    this.loadWorkouts(input.detail.value); // The parameters
+  }
+
+  async clear(){
+    console.log("Hello World");
+    //this.loadWorkouts(input.detail.value); // The parameters
   }
 
 }
--- zotfitness/src/app/service/search.service.ts
+++ zotfitness/src/app/service/search.service.ts
@@ -1,8 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-import { HttpClient } from '@angular/common/http'; //added {, HttpHeaders}
-import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export interface ApiResult {
+  name: string;
+  type: string;
+  muscle: string;
+  equipement: string;
+  difficulty: string;
+  instructions: string;
+}
+
 
 @Injectable({
   providedIn: 'root'
@@ -21,11 +30,45 @@ export class SearchService {
 
   //prints sample data from url 'https://api.api-ninjas.com/v1/exercises?type=strength&muscle=biceps'
   //'muscle=biceps', type=strength&muscle=biceps
-  getMuscleWorkouts():void{
+  getMuscleWorkouts(parameters: string):Observable<any>{
 //     console.log(this.sendRequestToExpress('biceps'));
-    this.sendRequestToExpress('type=strength&muscle=biceps').subscribe(data => {
-      console.log(data);
-    });
+    //this.sendRequestToExpress('type=strength&muscle=biceps').subscribe(data => {
+    //  console.log(data);
+    //});
+    return this.sendRequestToExpress(parameters);
   }
 
 }
+
+
+// import { Injectable } from '@angular/core';
+//
+// import { HttpClient } from '@angular/common/http'; //added {, HttpHeaders}
+// import { environment } from 'src/environments/environment';
+// import { Observable } from 'rxjs';
+//
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class SearchService {
+//
+//   constructor(private http:HttpClient) { }
+//
+//   //return sample data from calling baseURL in environments folder
+//   private sendRequestToExpress(endpoint: string):Observable<any> {
+//     let resp = this.http.get(`${environment.baseURL}${endpoint}`,
+// //       params: {muscle: 'biceps'},
+//       {headers: {'X-Api-Key': environment.apiKey}});
+//     return resp;
+//   }
+//
+//   //prints sample data from url 'https://api.api-ninjas.com/v1/exercises?type=strength&muscle=biceps'
+//   //'muscle=biceps', type=strength&muscle=biceps
+//   getMuscleWorkouts():void{
+// //     console.log(this.sendRequestToExpress('biceps'));
+//     this.sendRequestToExpress('type=strength&muscle=biceps').subscribe(data => {
+//       console.log(data);
+//     });
+//   }
+//
+// }
