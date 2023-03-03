e881fd58ae7851b126507dcabf8e56b31a28f9c2
Mindy Jun <mindyjvn@gmail.com>
2023-03-02 19:53:21 -0800

    Properly Saves data
    Uses Capacitor and IonChange to store muscles and goal. Modified .ts and .html to be cleaner/less lines

--- zotfitness/src/app/questions/questions.page.ts
+++ zotfitness/src/app/questions/questions.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { Router } from '@angular/router'; //added
+import { Preferences } from '@capacitor/preferences'; //added
 
 @Component({
   selector: 'app-questions',
@@ -9,11 +10,55 @@ import { Router } from '@angular/router'; //added
 })
 export class QuestionsPage implements OnInit {
 
+  goalCategory:string = "";
+  muscleCategory:string = "";
+
+  public muscles:string[] = ["Abdominals", "Abductors", "Calves", "Chest", "Forearms",
+    "Glutes", "Hamstrings", "Lats", "Lowerback", "Middleback", "Quadriceps",
+    "Traps", "Tripceps"];
+
   constructor(private router:Router) { }
 
   ngOnInit() {}
 
   onQuestionsClick() {
+    this.setGoal();
+    this.setMuscles();
+
     this.router.navigateByUrl('/search');
+
+    //this.removeGoal();
+    //this.removeMuscles();
   }
+
+  // event handler that updates muscleCategory whenever user selects a muscle
+  selectionMade(ev: Event){
+    this.muscleCategory = (ev.target as HTMLInputElement).value;
+  }
+
+  // function that deletes stored username
+  removeGoal = async () => {
+    await Preferences.remove({ key: 'goal' });
+  };
+
+  // function that deletes stored password
+  removeMuscles = async () => {
+    await Preferences.remove({ key: 'muscles' });
+  };
+
+  // function that sets goal with text from input box
+  setGoal = async () => {
+  await Preferences.set({
+    key: 'goal',
+    value: this.goalCategory,
+  });
+  };
+
+  // function that sets password with text from input box
+  setMuscles = async () => {
+  await Preferences.set({
+    key: 'muscles',
+    value: this.muscleCategory,
+  });
+  };
 }
