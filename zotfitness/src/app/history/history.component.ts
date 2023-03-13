fba5fce1cde80462b364a67662807eb016eb5631
worstdog <50923937+worstdog@users.noreply.github.com>
2023-03-12 17:05:57 -0700

    Update history.component.ts

--- zotfitness/src/app/history/history.component.ts
+++ zotfitness/src/app/history/history.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit,Output } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router'; //added
@@ -11,9 +11,20 @@ import { Router } from '@angular/router'; //added
   standalone: true,
 })
 export class HistoryComponent implements OnInit {
+  myVariable: string = "0"; 
 
+  @Output() myVariableChange = new EventEmitter<string>();
+  
   constructor(private router:Router) { }
 
   ngOnInit() {}
 
+  onInputChange(event: any) {
+    const inputValue: number = parseInt(event.target.value, 10);
+    if (!isNaN(inputValue)) {
+      this.myVariable = Math.round(inputValue * 60 / 31).toString();
+      this.myVariableChange.emit(this.myVariable);
+    }
+  }
+  
 }
