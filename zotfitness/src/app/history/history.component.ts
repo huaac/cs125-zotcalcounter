7dc6a01e44019049843ec45c45ea3cd232e5a240
huaac <huaac1@uci.edu>
2023-03-17 15:26:32 -0700

    Revert "Update history.component.ts"
    This reverts commit 8085aeac48954f5d85870d1ac1a8f8538e768092.

--- zotfitness/src/app/history/history.component.ts
+++ zotfitness/src/app/history/history.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, OnInit,Output } from '@angular/core';
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router'; //added
+import { ModalController } from '@ionic/angular'; // added
 
 @Component({
   imports: [IonicModule],
@@ -11,19 +12,11 @@ import { Router } from '@angular/router'; //added
   standalone: true,
 })
 export class HistoryComponent implements OnInit {
-  myVariable: string = "0";
+  myVariable: string = "0"; 
+
   @Output() myVariableChange = new EventEmitter<string>();
-
-  myVariable2: number = 0;
-  @Output() myVariable2Change = new EventEmitter<number>();
-  myVariable2_1: number = 0;
-  myVariable2_2: number = 0
-
-  totalMinutes: number = 0;
-  totalMinutes1: number = 0;
-  totalMinutes2: number = 0;
-
-  constructor(private router:Router) { }
+  
+  constructor(private router:Router, public modalCtrl:ModalController) { }
 
   ngOnInit() {}
 
@@ -34,28 +27,9 @@ export class HistoryComponent implements OnInit {
       this.myVariableChange.emit(this.myVariable);
     }
   }
-  
-  onMyVariable2ButtonClick(inputValue: string) {
-    const value: number = parseInt(inputValue, 10);
-    if (!isNaN(value)) {
-      this.myVariable2_2 = this.myVariable2_1;
-      this.myVariable2_1 = this.myVariable2;
 
-      this.myVariable2 = value;
-      this.myVariable2Change.emit(this.myVariable2);
-      
-      this.totalMinutes2 = this.totalMinutes1;
-      this.totalMinutes1 = this.totalMinutes;
-      this.updateTotalMinutes();
-    }
+  changePage(){
+    this.router.navigateByUrl('/checkin');
   }
-  
-  updateTotalMinutes() {
-    const myVariableNumber = parseInt(this.myVariable, 10);
-    if (!isNaN(myVariableNumber)) {
-      this.totalMinutes = parseFloat(((myVariableNumber * this.myVariable2) / 60).toFixed(1));
-    }
-  }
-  
   
 }
